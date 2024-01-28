6936cfc776a760dc869c1471cee9504cb3489c8f
Phạm Viết Tường <113772101+PhamVietTuong@users.noreply.github.com>
2024-01-28 20:48:20 +0700

    add model statistical

---
+++ BookStore/BookStore/Models/StatisticalViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace BookStore.Models
+{
+	public class StatisticalViewModel
+	{
+        public string Month { get; set; }
+
+        public double TotalToThis { get; set; }
+
+        public int BooksSoldCount { get; set; }
+    }
+}
