a98e796011d929e1729d2141f29a9570e77d3f7d
Nguyễn Nhật Huy <Nhathuy03122003@gmail.com>
2024-02-02 22:15:17 +0700

    FavouriteViewModel

---
+++ BookStore/BookStore/Models/FavouriteViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace BookStore.Models
+{
+	public class FavouriteViewModel
+	{
+		public int Id { get; set; }
+
+		public int BookId { get; set; }
+		public string Title { get; set; }
+
+		public double Price { get; set; }
+
+		public int Promotion { get; set; }
+
+		public double Start { get; set; }
+
+		public string ImageName { get; set; }
+	}
+}
